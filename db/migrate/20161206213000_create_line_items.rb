98933c54d5bf40d3e28c005593921658fd9c3ecb
amax3002 <alexandremguy@gmil.com>
2016-12-06 16:42:03 -0500

    updated total sale and estimated bake time

--- db/migrate/20161206213000_create_line_items.rb
+++ db/migrate/20161206213000_create_line_items.rb
@@ -4,8 +4,8 @@ class CreateLineItems < ActiveRecord::Migration[5.0]
       t.integer :order_id
       t.integer :product_id
       t.integer :quantity
-      t.integer :total_sale_price
-      t.integer :estimated_bake_time
+      t.integer :total_sale_price_in_cents
+      t.integer :estimated_bake_time_in_seconds
 
       t.timestamps
     end
