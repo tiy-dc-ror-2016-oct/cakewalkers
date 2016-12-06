656f473761982bdb6bde8d879e09dfb67f01536b
Ben Mangelsen <bmangelsen@gmail.com>
2016-12-06 16:48:09 -0500

    Updated order model

--- db/migrate/20161206212421_create_orders.rb
+++ db/migrate/20161206212421_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
-      t.integer :user_id
+      t.integer :client_id
       t.integer :cakewalker_id
       t.string :status
 
