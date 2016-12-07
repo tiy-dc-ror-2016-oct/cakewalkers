56a6113b0a0c2bcff94c120d0544288c566c8ff0
amax3002 <alexandremguy@gmil.com>
2016-12-07 14:46:50 -0500

    removed methods from User class that should be in client_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,24 +6,4 @@ class User < ApplicationRecord
   has_many :orders, foreign_key: :cakewalker_id
   validates :email, uniqueness: true
 
-  def new
-    @client = User.new
-  end
-
-  def create
-    @client = User.new(client_params)
-
-    if @client.save
-      session[:current_client_id] = @client.id
-      redirect_to productss_path
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def client_params
-    params.require(:client).permit(:email, :password_confirmation, :password)
-  end
 end
