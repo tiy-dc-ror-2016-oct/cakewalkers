29945e089b4b9e1df5d92dd15ae7d4a72c50954c
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-12-07 16:21:31 -0500

    Created and tested CRUD for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,12 @@ class Order < ApplicationRecord
   has_many :line_items
   belongs_to :client, class_name: :user
   belongs_to :cakewalker, class_name: :user
+  validates :billing_street, presence: true
+  validates :billing_city, presence: true
+  validates :billing_state, presence: true
+  validates :billing_zip, presence: true
+  validates :full_name, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
+
 end
