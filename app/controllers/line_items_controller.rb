1bc1c99be37364e2e515e70285b3adbb0cf9246a
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-12-08 22:02:26 -0500

    Added some small design changes and an endpoint to get status for a line item

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,4 +1,6 @@
 class LineItemsController < ApplicationController
+  BASE_URI = ENV["CAKEWALKER_API"]||"http://localhost:1234/bake_jobs"
+
   def create
     @products = Product.all
     @line_item = LineItem.new(line_item_params)
@@ -8,6 +10,11 @@ class LineItemsController < ApplicationController
     end
 
     redirect_to products_path
+  end
+
+  def status
+    @line_item = LineItem.find(params[:id])
+    HTTParty.get("#{BASE_URI}/bake_jobs/#{params[:id]}").to_json
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   resources :client_orders, only: [:show]
   resources :products
-  resources :line_items, except: [:show, :new]
+  resources :line_items, except: [:show, :new] do
+    member do
+      get '/status', action: :status, as: :status
+    end
+  end
   resource :session, only: [:new, :create, :destroy]
   resources :users, except: [:index]
   resources :orders, only: [:new, :create, :edit, :update, :destroy]
