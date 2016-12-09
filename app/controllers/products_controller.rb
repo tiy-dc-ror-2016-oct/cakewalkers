43812b185e44809a1676b36eab812ed80a90387a
amax3002 <alexandremguy@gmil.com>
2016-12-09 10:31:38 -0500

    trying to set up admin view to allow changes to viewable products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,23 @@ class ProductsController < ApplicationController
 
   def new
   end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(params[:id])
+    else
+      render :edit
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:unit_price_in_cents, :for_sale, :name)
+  end
+
 end
