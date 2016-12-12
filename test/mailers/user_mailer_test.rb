cb04005432317dec968a10cdbc96a35d22145704
amax3002 <alexandremguy@gmil.com>
2016-12-12 15:27:21 -0500

    remove coverage file

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test "appointment" do
-    mail = UsersMailer.appointment
-    assert_equal "Appointment", mail.subject
+    mail = UserMailer.welcome_email
+    assert_equal "Sign Up", mail.subject
     assert_equal ["to@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
     assert_match "Hi", mail.body.encoded
