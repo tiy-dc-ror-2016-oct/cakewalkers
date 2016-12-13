f49c97f24e288ebdf16b6a187f05cd7f40cc13bc
Ben Mangelsen <bmangelsen@gmail.com>
2016-12-13 08:57:52 -0500

    Fixed fixtures and some tests

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -2,18 +2,16 @@ require 'test_helper'
 
 class LineItemsControllerTest < ActionDispatch::IntegrationTest
   test "can create line item" do
-    post line_items_path, params: { }
+    get products_path
+    post line_items_path, params: { line_item: { quantity: 17 } }
+    assert_equal 17, LineItem.last.quantity
   end
 
-  # test "can delete line item" do
-  #
-  # end
-  #
-  # test "can get line item update view" do
-  #
-  # end
-  #
-  # test "can update line item" do
-  #
-  # end
+  test "can delete line item" do
+    get products_path
+    post line_items_path, params: { line_item: { quantity: 17 } }
+    assert_equal 6, LineItem.count
+    delete line_item_path(LineItem.last.id)
+    assert_equal 5, LineItem.count
+  end
 end
